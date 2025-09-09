8066d6c4aae37b72e83e548c4a8b873b865d9682
Jess <merukoo0507@gmail.com>
2025-09-09 14:42:17 +0800

    add flutter widget test for todo list

---
+++ test/mock_list_view_model.dart
@@ -0,0 +1,30 @@
+
+
+
+import 'package:fimber/fimber.dart';
+import 'package:flutter_todo_list_with_cicd/domain/model/todo.dart';
+import 'package:flutter_todo_list_with_cicd/pages/todo_list/model/todo_list_page_view_model_state.dart';
+import 'package:flutter_todo_list_with_cicd/pages/todo_list/todo_list_page_view_model.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class MockTodoListViewModel extends StateNotifier<TodoListPageViewModelState> implements TodoListPageViewmodel {
+  TodoListPageViewModelState mockState;
+
+  MockTodoListViewModel(this.mockState) : super(mockState);
+  
+  @override
+  Future<void> addTodo(Todo todo) async {
+    Fimber.d('addTodo: $todo');
+  }
+  
+  @override
+  Future<void> delete({required Todo todo}) async {
+    Fimber.d('addTodo: $todo');
+  }
+  
+  @override
+  Future<void> toggleCompleteStatus({required Todo todo}) async {
+    Fimber.d('toggleCompleteStatus: $todo');
+  }
+  
+}
